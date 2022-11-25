f1b8347df821efb16e915b5e82abb497abe410a5
xshiba <92438125+xshiba@users.noreply.github.com>
2022-11-25 21:31:42 +0700

    Update LOAD.lua

--- LOAD.lua
+++ LOAD.lua
@@ -21,4 +21,28 @@ task.spawn(function()
         end
         return old(...)
     end)
+end);
+task.spawn(function()
+    local gg = getrawmetatable(game)
+    local old = gg.__namecall
+    setreadonly(gg,false)
+    gg.__namecall = newcclosure(function(...)
+        local method = getnamecallmethod()
+        local args = {...}
+        if tostring(method) == "FireServer" then
+            if tostring(args[1]) == "RemoteEvent" then
+                if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
+                    if _G.Settings.AutoFarmGunMastery and StartMasteryGunMagnet then
+                        if tostring(typeof(args[2])) == "CFrame" then
+                            args[2] = PosMonMasteryGun
+                        elseif tostring(typeof(args[2])) == "Vector3" then
+                            args[2] = PosMonMasteryGun.Position
+                        end
+                        return old(unpack(args))
+                    end
+                end
+            end
+        end
+        return old(...)
+    end)
 end)
