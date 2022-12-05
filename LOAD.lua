70309cae55878c0775d5ffece06a628f61da4b08
xshiba <92438125+xshiba@users.noreply.github.com>
2022-12-05 17:53:12 +0700

    Update LOAD.lua

--- LOAD.lua
+++ LOAD.lua
@@ -117,6 +117,28 @@ task.spawn(function()
         end
         return old(...)
     end)
+    local gg = getrawmetatable(game)
+    local old = gg.__namecall
+    setreadonly(gg,false)
+    gg.__namecall = newcclosure(function(...)
+        local method = getnamecallmethod()
+        local args = {...}
+        if tostring(method) == "" then
+            if tostring(args[1]) == "RemoteEvent" then
+                if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
+                    if _G.Settings.AimbotSkills and MobP then
+                        if tostring(typeof(args[2])) == "CFrame" then
+                            args[2] = MobP
+                        elseif tostring(typeof(args[2])) == "Vector3" then
+                            args[2] = MobP.Position
+                        end
+                        return old(unpack(args))
+                    end
+                end
+            end
+        end
+        return old(...)
+    end)
 end)
 task.spawn(function()
     local gg = getrawmetatable(game)
@@ -126,6 +148,28 @@ task.spawn(function()
         local method = getnamecallmethod()
         local args = {...}
         if tostring(method) == "FireServer" then
+            if tostring(args[1]) == "RemoteEvent" then
+                if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
+                    if _G.Settings.AimbotSkills_Near and AimbotNearestSelectPosition then
+                        if tostring(typeof(args[2])) == "CFrame" then
+                            args[2] = AimbotNearestSelectPosition
+                        elseif tostring(typeof(args[2])) == "Vector3" then
+                            args[2] = AimbotNearestSelectPosition.Position
+                        end
+                        return old(unpack(args))
+                    end
+                end
+            end
+        end
+        return old(...)
+    end)
+    local gg = getrawmetatable(game)
+    local old = gg.__namecall
+    setreadonly(gg,false)
+    gg.__namecall = newcclosure(function(...)
+        local method = getnamecallmethod()
+        local args = {...}
+        if tostring(method) == "" then
             if tostring(args[1]) == "RemoteEvent" then
                 if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
                     if _G.Settings.AimbotSkills_Near and AimbotNearestSelectPosition then
